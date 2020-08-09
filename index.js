892fcf58f88d59b24b6970c8ac5f4de775cc7d4f
Ganapathy <54110633+GanapathyPT@users.noreply.github.com>
2020-08-09 23:42:08 +0530

    Fixed deploy port issue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const http = require("http").createServer(app);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const io = require("socket.io")(http);
 
